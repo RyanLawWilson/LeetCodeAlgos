a18b836315e390c71d592878dc8ed832ef03cc59
Ryan Wilson <ryan.law.wilson@gmail.com>
2021-10-27 17:45:57 -0600

    Add comments

--- LeetCodeAlgos/SinglyLinkedListNode.cs
+++ LeetCodeAlgos/SinglyLinkedListNode.cs
@@ -15,8 +15,11 @@ namespace LeetCodeAlgos
         }
 
         /// <summary>
-        /// Merge two linked lists into a single linked list.
+        /// Merge two linked lists into a single linked list.  Problem is from LeetCode
+        /// Linked Lists can be created by the ReturnRandomLinkedList method.
         /// </summary>
+        /// <link>https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/771/</link>
+        /// <see cref="ReturnRandomLinkedList"/>
         /// <param name="l1">Sorted linked list</param>
         /// <param name="l2">Sorted linked list</param>
         /// <returns>Merged sorted linked list</returns>
@@ -29,6 +32,8 @@ namespace LeetCodeAlgos
             SinglyLinkedListNode l1PlaceHolder = head == l1 ? l1.next : l1;
             SinglyLinkedListNode l2PlaceHolder = head == l2 ? l2.next : l2;
 
+            Console.WriteLine($"Head before iterating: {head}\n");
+
             // If one of the pointers is null, stop looping.
             while (l1PlaceHolder != null && l2PlaceHolder != null)
             {
@@ -37,25 +42,44 @@ namespace LeetCodeAlgos
                 // respective linked list.
                 if (l1PlaceHolder.val <= l2PlaceHolder.val)
                 {
+                    Console.WriteLine($"l1PlaceHolder value is smaller: {l1PlaceHolder.val} <= {l2PlaceHolder.val}");
+                    if (current.next == l1PlaceHolder)
+                        Console.WriteLine("No new node added to the list");
+
                     current.next = l1PlaceHolder;
                     l1PlaceHolder = l1PlaceHolder.next;
+                    
+                    Console.WriteLine($"List so far: {head}\n");
                 }
                 else
                 {
+                    Console.WriteLine($"l2PlaceHolder value is smaller: {l1PlaceHolder.val} > {l2PlaceHolder.val}");
+                    if (current.next == l2PlaceHolder)
+                        Console.WriteLine("No new node added to the list");
+
                     current.next = l2PlaceHolder;
                     l2PlaceHolder = l2PlaceHolder.next;
+                    Console.WriteLine($"List so far: {head}\n");
                 }
 
                 current = current.next;
             }
 
+            Console.WriteLine("One of the pointers is null so put the remaining nodes on the end.\n");
             // Find the pointer that is not null and set current.next to that node.
             current.next = l2PlaceHolder ?? l1PlaceHolder;
 
             return head;
         }
     
-        public static SinglyLinkedListNode ReturnRandomLinkedList(int length = 4, int minRange = 0, int maxRange = 10)
+        /// <summary>
+        /// Returns a node for a singly-linked list of a certain length.  The list is sorted in ascending order.
+        /// </summary>
+        /// <param name="length">The length of the list (number of nodes)</param>
+        /// <param name="minRange">The smallest value a node can have</param>
+        /// <param name="maxRange">The largest value a node can have</param>
+        /// <returns>A linked list node that represents a sorted lined list</returns>
+        public static SinglyLinkedListNode ReturnRandomLinkedList(int length = 4, int minRange = 0, int maxRange = 20)
         {
             if (length == 0) return null;
             length = length < 0 ? 4 : length;
@@ -79,7 +103,7 @@ namespace LeetCodeAlgos
         public override string ToString()
         {
             StringBuilder sb = new StringBuilder();
-            sb.Append("[ ");
+            sb.Append("[");
 
             SinglyLinkedListNode cur = this;
             while (cur != null)
@@ -92,10 +116,10 @@ namespace LeetCodeAlgos
             return sb.ToString();
         }
 
-        public string ToString(string delimeter = "->")
+        public string ToString(string delimeter = " -> ")
         {
             StringBuilder sb = new StringBuilder();
-            sb.Append("[ ");
+            sb.Append("[");
 
             SinglyLinkedListNode cur = this;
             while (cur != null)
